c594c339b87a3b0d7600071fcf3c48cb8eefad0c
Diego Ruiz <ruizg.diego@gmail.com>
2024-10-02 14:56:25 +0200

    #12 code refactor - generate xml

--- src/auler/gmbh/zugferdxinvoice/process/ZUGFeRD.java
+++ src/auler/gmbh/zugferdxinvoice/process/ZUGFeRD.java
@@ -27,34 +27,16 @@ package auler.gmbh.zugferdxinvoice.process;
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
 import org.adempiere.exceptions.AdempiereException;
-import org.compiere.model.MAttachment;
-import org.compiere.model.MBPartner;
-import org.compiere.model.MBPartnerLocation;
-import org.compiere.model.MBank;
-import org.compiere.model.MBankAccount;
-import org.compiere.model.MCharge;
-import org.compiere.model.MClient;
-import org.compiere.model.MCountry;
-import org.compiere.model.MDocType;
 import org.compiere.model.MInvoice;
-import org.compiere.model.MInvoiceLine;
-import org.compiere.model.MLocation;
-import org.compiere.model.MOrg;
 import org.compiere.model.MPaymentTerm;
-import org.compiere.model.MProduct;
 import org.compiere.model.MSysConfig;
-import org.compiere.model.MTax;
-import org.compiere.model.MUOM;
-import org.compiere.model.MUser;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
 import org.compiere.util.CLogger;
@@ -62,15 +44,8 @@ import org.compiere.util.DB;
 import org.compiere.util.Env;
 import org.compiere.util.Msg;
 import org.compiere.util.Trx;
-import org.mustangproject.BankDetails;
-import org.mustangproject.Contact;
-import org.mustangproject.Invoice;
-import org.mustangproject.Item;
-import org.mustangproject.Product;
-import org.mustangproject.TradeParty;
 import org.mustangproject.ZUGFeRD.IZUGFeRDPaymentDiscountTerms;
 import org.mustangproject.ZUGFeRD.IZUGFeRDPaymentTerms;
-import org.mustangproject.ZUGFeRD.ZUGFeRDExporterFromA1;
 
 
 public class ZUGFeRD extends SvrProcess {
@@ -138,192 +113,12 @@ public class ZUGFeRD extends SvrProcess {
     		throw new AdempiereException(Msg.getMsg(Env.getLanguage(getCtx()), "Insert POReference !"));
     	
     	//Bank Details
-    	MBank bank = new MBank(Env.getCtx(), c_Bank_ID, null);
-    	MBankAccount baccount = new MBankAccount(Env.getCtx(), c_BankAccount_ID, null);
-    	if((baccount.getIBAN()==null)||(bank.getRoutingNo()==null)||(bank.getName()==null))
+    	zugFerdGenerator.setBank(c_Bank_ID);
+    	zugFerdGenerator.setBankAccount(c_BankAccount_ID);
+    	if(!zugFerdGenerator.isValidBankDetail())
     		throw new AdempiereException(Msg.getMsg(Env.getLanguage(getCtx()), "Check your Bankdetails !"));
     	
-    	MDocType docType = MDocType.get(m_invoice.getC_DocType_ID());
-    	boolean isARC = (docType.getDocBaseType().equals(MInvoice.DOCBASETYPE_ARCreditMemo));
-    	
-    	ZUGFeRDExporterFromA1 ze = new ZUGFeRDExporterFromA1();
-    	
-    	MClient client = new MClient(Env.getCtx(), Env.getAD_Client_ID(getCtx()), null);
-    	
-    	ze.setProducer(client.getName());
-    	MUser invoiceUser = MUser.get(m_invoice.getAD_User_ID());
-    	ze.setCreator(((invoiceUser.getBPName()==null)?"":invoiceUser.getBPName()));
-
-    	ze.ignorePDFAErrors();
-    	ze.load(printfile.getAbsolutePath());
-    	
-    	log.info(printfile.getAbsolutePath());
-    	
-    	//Invoice
-    	Invoice invoice = new Invoice();
-    	
-    	patpaymentterms pt = new patpaymentterms(m_invoice);
-    	
-    	invoice.setPaymentTerms(pt);
-    	
-    	Timestamp duedate = DB.getSQLValueTSEx(m_invoice.get_TrxName(), sqlduedate, m_invoice.getC_PaymentTerm_ID(), m_invoice.getDateInvoiced());
-
-    	invoice.setDueDate(duedate);
-    	MPaymentTerm paymentTerm = new MPaymentTerm(getCtx(), m_invoice.getC_PaymentTerm_ID(), get_TrxName());
-    	invoice.setPaymentTermDescription(paymentTerm.getName());
-    	invoice.setIssueDate(m_invoice.getDateInvoiced());
-    	invoice.setDeliveryDate(m_invoice.getDateInvoiced());
-    	invoice.setNumber(m_invoice.getDocumentNo());
-    	
-    	MOrg org = new MOrg(Env.getCtx(), m_invoice.getAD_Org_ID(), null);
-    	StringBuilder addressSender = new StringBuilder();
-    	MLocation orgLocation = MLocation.get(org.getInfo().getC_Location_ID());
-    	if(orgLocation.getAddress1()!=null)
-    		addressSender.append(orgLocation.getAddress1());
-    	if(orgLocation.getAddress2()!=null)
-    		addressSender.append(orgLocation.getAddress2());
-    	if(orgLocation.getAddress3()!=null)
-    		addressSender.append(orgLocation.getAddress3());
-    	if(orgLocation.getAddress4()!=null)
-    		addressSender.append(orgLocation.getAddress4());
-
-    	MCountry orgCountry = MCountry.get(orgLocation.getC_Country_ID());
-    	TradeParty tradePartySender = new TradeParty(client.getName(), 
-    			addressSender.toString(), 
-    			orgLocation.getPostal(), 
-    			orgLocation.getCity(), 
-    			orgCountry.getCountryCode());
-    	
-    	tradePartySender.addVATID(org.getInfo().getTaxID());
-    	    	
-    	BankDetails bankd = new BankDetails(baccount.getIBAN(), bank.getRoutingNo());
-    	bankd.setAccountName(bank.getName());
-
-    	tradePartySender.addBankDetails(bankd);
-    	
-    	StringBuilder addressRecipient = new StringBuilder();
-    	
-    	MBPartnerLocation bpLocation = new MBPartnerLocation(getCtx(), m_invoice.getC_BPartner_Location_ID(), null);
-    	MLocation location = bpLocation.getLocation(true);
-    	if(location.getAddress1()!=null)
-    		addressRecipient.append(location.getAddress1());
-    	if(location.getAddress2()!=null)
-    		addressRecipient.append(location.getAddress2());
-    	if(location.getAddress3()!=null)
-    		addressRecipient.append(location.getAddress3());
-    	if(location.getAddress4()!=null)
-    		addressRecipient.append(location.getAddress4());
-
-    	MBPartner bp = MBPartner.get(getCtx(), m_invoice.getC_BPartner_ID());
-    	MCountry bpCountry = MCountry.get(location.getC_Country_ID());
-    	TradeParty tradePartyRecipient = new TradeParty(
-    			bp.getName()
-    			+ ((bp.getName2()==null)?"":", "+ bp.getName2()), 
-    			addressRecipient.toString(),
-    			(location.getPostal()==null)?"":location.getPostal(),
-    			(location.getCity()==null)?"":location.getCity(),
-    			(bpCountry.getCountryCode()==null)?"":bpCountry.getCountryCode()
-    			);
-    	tradePartyRecipient.addVATID(bp.getTaxID());
-    	
-    	Contact contact = new Contact((invoiceUser.getName()==null)?"":invoiceUser.getName(), 
-    			(invoiceUser.getPhone()==null)?"":invoiceUser.getPhone(),
-    			(invoiceUser.getEMail()==null)?"":invoiceUser.getEMail());
-    	tradePartyRecipient.setContact(contact);
-
-    	invoice.setSender(tradePartySender);
-    	invoice.setRecipient(tradePartyRecipient);
-    	
-    	//Leitweg-ID
-    	invoice.setReferenceNumber(m_invoice.getPOReference());
-
-//    	invoice.setBuyerOrderReferencedDocumentID("");
-//    	invoice.setBuyerOrderReferencedDocumentIssueDateTime(""); 
-    	
-    	
-		for (MInvoiceLine invoiceLine : m_invoice.getLines()) {
-
-			Item item = new Item();
-
-			MUOM unitOfMeasure = MUOM.get(invoiceLine.getC_UOM_ID());
-			String uom = unitOfMeasure.getUNCEFACT();
-
-			if (invoiceLine.isDescription() || (invoiceLine.getM_Product_ID() == 0 && invoiceLine.getC_Charge_ID() == 0)) {
-
-				Product product = new Product();
-				product.setName("Descriptionline");
-				product.setDescription((invoiceLine.getDescription()==null?"":invoiceLine.getDescription()));
-				product.setVATPercent(Env.ZERO);
-				product.setUnit(CD_UOM);
-				
-				item.setProduct(product);
-				item.setQuantity(Env.ZERO);
-				item.setPrice(Env.ZERO);
-				item.setTax(Env.ZERO);
-				item.setLineTotalAmount(Env.ZERO);
-				
-				invoice.addItem(item);
-
-				
-			} else if (invoiceLine.getM_Product_ID() > 0) {
-				
-				Product product = new Product();
-				MProduct productLine = MProduct.get(invoiceLine.getM_Product_ID());
-				product.setName(productLine.getValue());
-				product.setDescription((invoiceLine.getDescription()==null?"":invoiceLine.getDescription()));
-				MTax tax = MTax.get(invoiceLine.getC_Tax_ID());
-				product.setVATPercent(tax.getRate());
-				product.setUnit(uom);
-				product.setSellerAssignedID(productLine.getValue());
-
-				item.setProduct(product);
-				if(isARC)
-					item.setQuantity(invoiceLine.getQtyInvoiced().negate().setScale(2, RoundingMode.HALF_UP));
-				else
-					item.setQuantity(invoiceLine.getQtyInvoiced().setScale(2, RoundingMode.HALF_UP));
-				item.setPrice(invoiceLine.getPriceActual().setScale(2, RoundingMode.HALF_UP));
-				item.setTax(invoiceLine.getTaxAmt().setScale(2, RoundingMode.HALF_UP));
-				item.setLineTotalAmount(invoiceLine.getLineTotalAmt().setScale(2, RoundingMode.HALF_UP));
-				invoice.addItem(item);
-				
-			} else if (invoiceLine.getC_Charge_ID() > 0) {
-				
-				Product product = new Product();
-				MCharge charge = MCharge.get(invoiceLine.getC_Charge_ID());
-				product.setName(charge.getName());
-				product.setDescription((invoiceLine.getDescription()==null?"":invoiceLine.getDescription()));
-				MTax tax = MTax.get(invoiceLine.getC_Tax_ID());
-				product.setVATPercent(tax.getRate());
-				product.setUnit(CD_UOM);
-
-				item.setProduct(product);
-				if(isARC)
-					item.setQuantity(invoiceLine.getQtyInvoiced().negate().setScale(2, RoundingMode.HALF_UP));
-				else
-					item.setQuantity(invoiceLine.getQtyInvoiced().setScale(2, RoundingMode.HALF_UP));
-				item.setPrice(invoiceLine.getPriceActual().setScale(2, RoundingMode.HALF_UP));
-				item.setTax(invoiceLine.getTaxAmt().setScale(2, RoundingMode.HALF_UP));
-				item.setLineTotalAmount(invoiceLine.getLineTotalAmt().setScale(2, RoundingMode.HALF_UP));
-				invoice.addItem(item);
-							
-			}
-			
-		}
-		
-		ze.setTransaction(invoice);
-		ze.export(printfile.getAbsolutePath());
-		ze.close();
-
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-		File printdstfile = new File(printfile.getParent()
-				+ "/" + m_invoice.getDocumentNo() +"-" 
-				+ sdf.format(m_invoice.getDateInvoiced())
-				+ "." + FILE_SUFFIX);
-		printfile.renameTo(printdstfile);
-		
-		MAttachment atmt = m_invoice.createAttachment();
-		atmt.addEntry(printdstfile);
-		atmt.saveEx();
+    	zugFerdGenerator.generateAndEmbeddXML(printfile);
 
     }
     
--- src/auler/gmbh/zugferdxinvoice/process/ZugFerdGenerator.java
+++ src/auler/gmbh/zugferdxinvoice/process/ZugFerdGenerator.java
@@ -22,20 +22,56 @@
 package auler.gmbh.zugferdxinvoice.process;
 
 import java.io.File;
+import java.io.IOException;
+import java.math.RoundingMode;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
+import org.compiere.model.MAttachment;
+import org.compiere.model.MBPartner;
+import org.compiere.model.MBPartnerLocation;
+import org.compiere.model.MBank;
+import org.compiere.model.MBankAccount;
+import org.compiere.model.MCharge;
+import org.compiere.model.MClient;
+import org.compiere.model.MCountry;
+import org.compiere.model.MDocType;
 import org.compiere.model.MInvoice;
+import org.compiere.model.MInvoiceLine;
+import org.compiere.model.MLocation;
+import org.compiere.model.MOrg;
+import org.compiere.model.MPaymentTerm;
+import org.compiere.model.MProduct;
+import org.compiere.model.MTax;
+import org.compiere.model.MUOM;
+import org.compiere.model.MUser;
 import org.compiere.print.MPrintFormat;
 import org.compiere.print.ReportEngine;
 import org.compiere.process.ProcessInfo;
 import org.compiere.process.ServerProcessCtl;
 import org.compiere.util.CLogger;
+import org.compiere.util.DB;
 import org.compiere.util.Env;
+import org.mustangproject.BankDetails;
+import org.mustangproject.Contact;
+import org.mustangproject.Invoice;
+import org.mustangproject.Item;
+import org.mustangproject.Product;
+import org.mustangproject.TradeParty;
+import org.mustangproject.ZUGFeRD.ZUGFeRDExporterFromA1;
+
+import auler.gmbh.zugferdxinvoice.process.ZUGFeRD.patpaymentterms;
 
 public class ZugFerdGenerator {
 
 	private CLogger log = CLogger.getCLogger(ZugFerdGenerator.class);
+	final static String sqlduedate = "SELECT PaymentTermDueDate(?,?) FROM Dual";
+	String CD_UOM="C62";
+	String FILE_SUFFIX="pdf";
 
 	private MInvoice invoice;
+	private MBank bank;
+	private MBankAccount bankAccount;
 
 	public ZugFerdGenerator(MInvoice invoice) {
 		this.invoice = invoice;
@@ -59,6 +95,206 @@ public class ZugFerdGenerator {
 		log.info("PDF Created: " + pdfFile.getName());
 
 		return pdfFile;
+	}
+	
+	public void setBank(int C_Bank_ID) {
+		bank = MBank.get(C_Bank_ID);
+	}
+
+	public void setBankAccount(int C_BankAccount_ID) {
+		bankAccount = MBankAccount.get(C_BankAccount_ID);
+		if (bankAccount != null && bank == null)
+			setBank(bankAccount.getC_Bank_ID());
+	}
+	
+	public boolean isValidBankDetail() {
+		return bank != null && bankAccount != null && 
+				bankAccount.getIBAN() != null && 
+				bank.getRoutingNo() != null && bank.getName() != null;
+	}
+	
+	public void generateAndEmbeddXML(File pdfFile) throws IOException {
+		MDocType docType = MDocType.get(invoice.getC_DocType_ID());
+    	boolean isARC = (docType.getDocBaseType().equals(MInvoice.DOCBASETYPE_ARCreditMemo));
+    	
+    	ZUGFeRDExporterFromA1 ze = new ZUGFeRDExporterFromA1();
+    	
+    	MClient client = new MClient(Env.getCtx(), Env.getAD_Client_ID(Env.getCtx()), null);
+    	
+    	ze.setProducer(client.getName());
+    	MUser invoiceUser = MUser.get(invoice.getAD_User_ID());
+    	ze.setCreator(((invoiceUser.getBPName()==null)?"":invoiceUser.getBPName()));
+
+    	ze.ignorePDFAErrors();
+    	ze.load(pdfFile.getAbsolutePath());
+    	
+    	log.info(pdfFile.getAbsolutePath());
+    	
+    	//Invoice
+    	Invoice zugFerdInvoice = new Invoice();
+    	
+    	patpaymentterms pt = new patpaymentterms(invoice);
+    	
+    	zugFerdInvoice.setPaymentTerms(pt);
+    	
+    	Timestamp duedate = DB.getSQLValueTSEx(invoice.get_TrxName(), sqlduedate, invoice.getC_PaymentTerm_ID(), invoice.getDateInvoiced());
+
+    	zugFerdInvoice.setDueDate(duedate);
+    	MPaymentTerm paymentTerm = new MPaymentTerm(Env.getCtx(), invoice.getC_PaymentTerm_ID(), invoice.get_TrxName());
+    	zugFerdInvoice.setPaymentTermDescription(paymentTerm.getName());
+    	zugFerdInvoice.setIssueDate(invoice.getDateInvoiced());
+    	zugFerdInvoice.setDeliveryDate(invoice.getDateInvoiced());
+    	zugFerdInvoice.setNumber(invoice.getDocumentNo());
+    	
+    	MOrg org = new MOrg(Env.getCtx(), invoice.getAD_Org_ID(), null);
+    	StringBuilder addressSender = new StringBuilder();
+    	MLocation orgLocation = MLocation.get(org.getInfo().getC_Location_ID());
+    	if(orgLocation.getAddress1()!=null)
+    		addressSender.append(orgLocation.getAddress1());
+    	if(orgLocation.getAddress2()!=null)
+    		addressSender.append(orgLocation.getAddress2());
+    	if(orgLocation.getAddress3()!=null)
+    		addressSender.append(orgLocation.getAddress3());
+    	if(orgLocation.getAddress4()!=null)
+    		addressSender.append(orgLocation.getAddress4());
+
+    	MCountry orgCountry = MCountry.get(orgLocation.getC_Country_ID());
+    	TradeParty tradePartySender = new TradeParty(client.getName(), 
+    			addressSender.toString(), 
+    			orgLocation.getPostal(), 
+    			orgLocation.getCity(), 
+    			orgCountry.getCountryCode());
+    	
+    	tradePartySender.addVATID(org.getInfo().getTaxID());
+    	    	
+    	BankDetails bankd = new BankDetails(bankAccount.getIBAN(), bank.getRoutingNo());
+    	bankd.setAccountName(bank.getName());
+
+    	tradePartySender.addBankDetails(bankd);
+    	
+    	StringBuilder addressRecipient = new StringBuilder();
+    	
+    	MBPartnerLocation bpLocation = new MBPartnerLocation(Env.getCtx(), invoice.getC_BPartner_Location_ID(), null);
+    	MLocation location = bpLocation.getLocation(true);
+    	if(location.getAddress1()!=null)
+    		addressRecipient.append(location.getAddress1());
+    	if(location.getAddress2()!=null)
+    		addressRecipient.append(location.getAddress2());
+    	if(location.getAddress3()!=null)
+    		addressRecipient.append(location.getAddress3());
+    	if(location.getAddress4()!=null)
+    		addressRecipient.append(location.getAddress4());
+
+    	MBPartner bp = MBPartner.get(Env.getCtx(), invoice.getC_BPartner_ID());
+    	MCountry bpCountry = MCountry.get(location.getC_Country_ID());
+    	TradeParty tradePartyRecipient = new TradeParty(
+    			bp.getName()
+    			+ ((bp.getName2()==null)?"":", "+ bp.getName2()), 
+    			addressRecipient.toString(),
+    			(location.getPostal()==null)?"":location.getPostal(),
+    			(location.getCity()==null)?"":location.getCity(),
+    			(bpCountry.getCountryCode()==null)?"":bpCountry.getCountryCode()
+    			);
+    	tradePartyRecipient.addVATID(bp.getTaxID());
+    	
+    	Contact contact = new Contact((invoiceUser.getName()==null)?"":invoiceUser.getName(), 
+    			(invoiceUser.getPhone()==null)?"":invoiceUser.getPhone(),
+    			(invoiceUser.getEMail()==null)?"":invoiceUser.getEMail());
+    	tradePartyRecipient.setContact(contact);
+
+    	zugFerdInvoice.setSender(tradePartySender);
+    	zugFerdInvoice.setRecipient(tradePartyRecipient);
+    	
+    	//Leitweg-ID
+    	zugFerdInvoice.setReferenceNumber(invoice.getPOReference());
+
+//    	invoice.setBuyerOrderReferencedDocumentID("");
+//    	invoice.setBuyerOrderReferencedDocumentIssueDateTime(""); 
+    	
+    	
+		for (MInvoiceLine invoiceLine : invoice.getLines()) {
+
+			Item item = new Item();
+
+			MUOM unitOfMeasure = MUOM.get(invoiceLine.getC_UOM_ID());
+			String uom = unitOfMeasure.getUNCEFACT();
+
+			if (invoiceLine.isDescription() || (invoiceLine.getM_Product_ID() == 0 && invoiceLine.getC_Charge_ID() == 0)) {
+
+				Product product = new Product();
+				product.setName("Descriptionline");
+				product.setDescription((invoiceLine.getDescription()==null?"":invoiceLine.getDescription()));
+				product.setVATPercent(Env.ZERO);
+				product.setUnit(CD_UOM);
+				
+				item.setProduct(product);
+				item.setQuantity(Env.ZERO);
+				item.setPrice(Env.ZERO);
+				item.setTax(Env.ZERO);
+				item.setLineTotalAmount(Env.ZERO);
+				
+				zugFerdInvoice.addItem(item);
+
+				
+			} else if (invoiceLine.getM_Product_ID() > 0) {
+				
+				Product product = new Product();
+				MProduct productLine = MProduct.get(invoiceLine.getM_Product_ID());
+				product.setName(productLine.getValue());
+				product.setDescription((invoiceLine.getDescription()==null?"":invoiceLine.getDescription()));
+				MTax tax = MTax.get(invoiceLine.getC_Tax_ID());
+				product.setVATPercent(tax.getRate());
+				product.setUnit(uom);
+				product.setSellerAssignedID(productLine.getValue());
+
+				item.setProduct(product);
+				if(isARC)
+					item.setQuantity(invoiceLine.getQtyInvoiced().negate().setScale(2, RoundingMode.HALF_UP));
+				else
+					item.setQuantity(invoiceLine.getQtyInvoiced().setScale(2, RoundingMode.HALF_UP));
+				item.setPrice(invoiceLine.getPriceActual().setScale(2, RoundingMode.HALF_UP));
+				item.setTax(invoiceLine.getTaxAmt().setScale(2, RoundingMode.HALF_UP));
+				item.setLineTotalAmount(invoiceLine.getLineTotalAmt().setScale(2, RoundingMode.HALF_UP));
+				zugFerdInvoice.addItem(item);
+				
+			} else if (invoiceLine.getC_Charge_ID() > 0) {
+				
+				Product product = new Product();
+				MCharge charge = MCharge.get(invoiceLine.getC_Charge_ID());
+				product.setName(charge.getName());
+				product.setDescription((invoiceLine.getDescription()==null?"":invoiceLine.getDescription()));
+				MTax tax = MTax.get(invoiceLine.getC_Tax_ID());
+				product.setVATPercent(tax.getRate());
+				product.setUnit(CD_UOM);
+
+				item.setProduct(product);
+				if(isARC)
+					item.setQuantity(invoiceLine.getQtyInvoiced().negate().setScale(2, RoundingMode.HALF_UP));
+				else
+					item.setQuantity(invoiceLine.getQtyInvoiced().setScale(2, RoundingMode.HALF_UP));
+				item.setPrice(invoiceLine.getPriceActual().setScale(2, RoundingMode.HALF_UP));
+				item.setTax(invoiceLine.getTaxAmt().setScale(2, RoundingMode.HALF_UP));
+				item.setLineTotalAmount(invoiceLine.getLineTotalAmt().setScale(2, RoundingMode.HALF_UP));
+				zugFerdInvoice.addItem(item);
+							
+			}
+			
+		}
+		
+		ze.setTransaction(zugFerdInvoice);
+		ze.export(pdfFile.getAbsolutePath());
+		ze.close();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		File printdstfile = new File(pdfFile.getParent()
+				+ "/" + invoice.getDocumentNo() +"-" 
+				+ sdf.format(invoice.getDateInvoiced())
+				+ "." + FILE_SUFFIX);
+		pdfFile.renameTo(printdstfile);
+		
+		MAttachment atmt = invoice.createAttachment();
+		atmt.addEntry(printdstfile);
+		atmt.saveEx();
 	}
 
 }
